e661ac9019c8f98aabe22bdafbf11d3df1e4b733
andruwdarma <andruwdarma772@gmail.com>
2022-04-19 20:53:01 +0700

    add menu

--- src/Package6PM/Driver6PM.java
+++ src/Package6PM/Driver6PM.java
@@ -21,5 +21,47 @@ public class Driver6PM {
         //System.out.println(t1.getIdTeam());
         //System.out.println(t2.getIdTeam());
         System.out.println("DONE");
+        menu();
+    }
+    
+    public static void menu(){
+        String ulg = "y";
+        while (ulg.equals("y"))
+        {
+        System.out.println(" ");
+        Scanner input = new Scanner(System.in);
+        String ul="",lagi;
+        System.out.println("********** SELAMAT DATANG ***********");
+        System.out.println(" ");
+        System.out.println("             MENU UTAMA             ");
+        System.out.println("1. menu1");
+        System.out.println("2. menu2");
+        System.out.println("3. menu3");
+        System.out.println("4. Keluar");
+        System.out.println("=====================================");
+        System.out.print("Pilihan [1/2/3/4] = ");
+        int pilih = input.nextInt();
+        System.out.println("=====================================");
+        switch (pilih) {
+            case 1:{
+                break;
+            }
+            case 2:{
+                break;
+            }
+             case 3:{
+                break;
+             }
+            case 4:{
+                System.exit(4);
+                System.out.println("Warning !! Pilihan tidak tersedia !! ");
+            }
+                break;
+        }
+        System.out.println("=====================================");
+        System.out.print("Apakah anda ingin mengulang (y/t)? ");
+        ulg = input.next();
+        }
     }
 }
+
